523960dcb5add86cb32f81b54f9ffb2b2b7cbb96
mtbriones <mikhelbriones@gmail.com>
2013-07-09 17:25:23 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
+var fs	= require('fs');
+var INPUT = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(INPUT).toString());
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
